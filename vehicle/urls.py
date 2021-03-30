e40b4424fa3e7a61a2e406c562c8b7680fbdaeec
hskm07 <yasonomazon75@gamil.com>
2021-03-30 22:07:13 +0900

    Revert "First commit"
    This reverts commit 0a9ee3ed5f60dece3d3ffc5db0b282dac52cfecb.

--- vehicle/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'vehicle'
-urlpatterns = [
-    path('predict/', views.predict, name='predict'),
-    path('', views.index, name='index'),
-    
-]
